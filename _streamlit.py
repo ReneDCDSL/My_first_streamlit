599d90ac56ce18194bd002837ce5bcfadde6b1df
Rene <renedcdsl@hotmail.fr>
2023-11-28 13:32:48 +0100

    updated streamlit

--- _streamlit.py
+++ _streamlit.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from PIL import Image
 import requests
+
+st.title("Poubelle Intelligente")
+
+c1, c2 = st.columns(2)
 
 # Streamlit code for file upload
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
@@ -11,12 +16,23 @@ if uploaded_file is not None:
     # Make a request to the FastAPI endpoint for prediction
     # Replace 'http://your-fastapi-server:8000' with the actual address of your FastAPI server
     url = "https://myfirstapp-v3.streamlit.app/predict"
-    files = {"file": uploaded_file}
+    files = {"file": (upload.name, upload.read(), upload.type)}    
     
     try:
         response = requests.post(url, files=files)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        resultat = response.json()
+        print(resultat)
+        rec = resultat["predictions"]
+        prob_recyclable = rec * 100      
+        prob_organic = (1-rec)*100
 
-    
     except requests.exceptions.RequestException as e:
-        st.error(f"Error making prediction request: {e}")
+        st.error(f"Error making prediction request: {e}")  
+        
+    c1.image(Image.open(upload))
+    if prob_recyclable > 50:
+        c2.write(f"Je suis certain à {prob_recyclable:.2f} % que l'objet est recyclable")
+    else:
+        c2.write(f"Je suis certain à {prob_organic:.2f} % que l'objet n'est pas recyclable")
+
+
