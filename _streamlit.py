8a74774e79e592ccd59a51f9591ecad85c50b1fe
Rene <renedcdsl@hotmail.fr>
2023-11-28 15:32:26 +0100

    updated streamlit

--- _streamlit.py
+++ _streamlit.py
@@ -12,7 +12,7 @@ c1, c2 = st.columns(2)
 
 if upload:
     files = {"file": (upload.name, upload.read(), upload.type)}
-    req = requests.post("https://renedcdsl.myfirstapp-v3.streamlit.app/predict", files=files, verify=False)
+    req = requests.post("https://share.streamlit.io/renedcdsl/myfirstapp-v3/predict", files=files, verify=False)
     resultat = req.json()
     print(resultat)
     rec = resultat["predictions"]
