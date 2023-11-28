b99ccad853b4569a484b8f20388c5299a8d2bb7e
Rene <renedcdsl@hotmail.fr>
2023-11-28 16:17:41 +0100

    updated streamlit

--- _streamlit.py
+++ _streamlit.py
@@ -1,23 +1,32 @@
 import streamlit as st
 from PIL import Image
-import requests
+import numpy as np
+from tensorflow.keras.models import load_model
+import io
 
+# Load your model
+model_path = "best_model.h5"
+model = load_model(model_path, compile=False)
 
 st.title("Poubelle Intelligente")
 
-upload = st.file_uploader("Chargez l'image de votre objet",
-                           type=['png', 'jpeg', 'jpg'])
+upload = st.file_uploader("Chargez l'image de votre objet", type=['png', 'jpeg', 'jpg'])
 
 c1, c2 = st.columns(2)
 
 if upload:
-    files = {"file": (upload.name, upload.read(), upload.type)}
-    req = requests.post("https://share.streamlit.io/renedcdsl/myfirstapp-v3/predict", files=files, verify=False)
-    resultat = req.json()
-    print(resultat)
-    rec = resultat["predictions"]
+    # Preprocess the image
+    img = Image.open(upload)
+    img = img.resize((224, 224))
+    img = np.asarray(img)
+    img = np.expand_dims(img, axis=0)
+
+    # Make prediction
+    predictions = model.predict(img)
+    rec = predictions[0][0].tolist()
+    
     prob_recyclable = rec * 100      
-    prob_organic = (1-rec)*100
+    prob_organic = (1 - rec) * 100
 
     c1.image(Image.open(upload))
     if prob_recyclable > 50:
