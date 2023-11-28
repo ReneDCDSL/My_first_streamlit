66e1530ff3d0473109c2a91639d88fccbeecb1c7
Rene <renedcdsl@hotmail.fr>
2023-11-28 04:01:58 +0100

    earlier version improved by gpt

--- _streamlit.py
+++ _streamlit.py
@@ -4,29 +4,20 @@ import requests
 
 st.title("Poubelle Intelligente")
 
-upload = st.file_uploader("Chargez l'image de votre objet",
-                           type=['png', 'jpeg', 'jpg'])
-
+upload = st.file_uploader("Chargez l'image de votre objet", type=['png', 'jpeg', 'jpg'])
 c1, c2 = st.columns(2)
 
 if upload:
     files = {"file": (upload.name, upload.read(), upload.type)}
     req = requests.post("https://myfirstapp-v3.streamlit.app/predict", files=files)
 
-    if req.status_code == 200:
-        try:
-            resultat = req.json()
-            rec = resultat["predictions"]
-            prob_recyclable = rec * 100
-            prob_organic = (1 - rec) * 100
+    try:
+        req.raise_for_status()
+        resultat = req.json()
+        rec = resultat["predictions"]
+        prob_recyclable = rec * 100
+        prob_organic = (1 - rec) * 100
 
-            c1.image(Image.open(upload))
-            if prob_recyclable > 50:
-                c2.write(f"Je suis certain à {prob_recyclable:.2f}% que l'objet est recyclable")
-            else:
-                c2.write(f"Je suis certain à {prob_organic:.2f}% que l'objet n'est pas recyclable")
-        except ValueError:
-            st.error("Invalid JSON received from the server.")
-    else:
-        st.error(f"Error making prediction request. Server returned status code: {req.status_code}\n"
-                 f"Response content: {req.text}")
+        c1.image(Image.open(upload))
+        if prob_recyclable > 50:
+            c2.write(f"Je suis certain à {prob_recyclab
