1e0c2eeededd9b098a4eccbfc2d2f6ef61c041bd
Rene <renedcdsl@hotmail.fr>
2023-11-28 13:35:53 +0100

    updated streamlit

--- _streamlit.py
+++ _streamlit.py
@@ -19,17 +19,5 @@ if uploaded_file is not None:
     files = {"file": (uploaded_file.name, uploaded_file.read(), uploaded_file.type)}    
     
     response = requests.post(url, files=files)
-    resultat = response.json()
-    print(resultat)
-    rec = resultat["predictions"]
-    prob_recyclable = rec * 100      
-    prob_organic = (1-rec)*100
-
-        
-    c1.image(Image.open(uploaded_file))
-    if prob_recyclable > 50:
-        c2.write(f"Je suis certain à {prob_recyclable:.2f} % que l'objet est recyclable")
-    else:
-        c2.write(f"Je suis certain à {prob_organic:.2f} % que l'objet n'est pas recyclable")
-
-
+    # Display the prediction
+    st.json(response.json())    
